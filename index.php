7505da85dc10e200f7792fbb6f931a3ec5e16099
ashdejesus <dejesusnichoeashley81@gmail.com>
2024-09-15 10:24:14 +0800

    SPRINT 2
     Create a from that will take inputs
     Task Title - Text input
    Task Description - Paragraph Input
    Task Due - Date input (format: yyyy/mm/dd)

--- index.php
+++ index.php
@@ -13,6 +13,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Task Management System</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 
 <body>
@@ -24,7 +25,24 @@
   </nav>
 
 
-  <p><button id="add-task-button">Add Task</button></p>
+  <button id="addTaskButton">Add Task</button>
+  <div id="taskForm" class="popup">
+        <h2>Add New Task</h2>
+        <label for="taskTitle">Title:</label>
+        <input type="text" id="taskTitle" name="taskTitle"><br><br>
+
+        <label for="taskDescription">Description:</label>
+        <textarea id="taskDescription" name="taskDescription"></textarea><br><br>
+
+        <label for="dueDate">Due Date:</label>
+        <input type="date" id="dueDate" name="dueDate"><br><br>
+
+        <button id="saveTaskButton">Save Task</button>
+        <button id="closeTaskForm">Close</button>
+    </div>
+        
+        <script src="script.js"></script>
+    
 
 
 <table>
